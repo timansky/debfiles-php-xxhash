ac9366a106456dc8a38a376e3f2332c4cb65ac46
root <root@timansky.sandbox.alahd.kz.dev.bash.kz>
2019-08-02 10:44:44 +0600

    Add build scripts

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+set -x
+
+# APT params
+_APT_PROXY=${APT_PROXY}
+_APT_FLAGS_COMMON=${APT_FLAGS_COMMON:=-y --no-install-recommends}
+export DEBIAN_FRONTEND=noninteractive
+
+_ROOT_DIR=$(pwd)
+
+_MAIN_PACKAGES=" \
+                curl \
+                devscripts \
+                equivs \
+                software-properties-common \
+"
+
+# Proc numbers for building faster
+_NB_PROC=${NB_PROC:=$(grep -c ^processor /proc/cpuinfo)}
+
+echo "Cleaning..."
+# Clean out any files from previous runs of this script
+rm -rf build
+
+# Ensure that we have the required software to compile
+if [ -z "${_APT_PROXY}" ]; then
+  echo "Acquire::http::Proxy \"{{ ${_APT_PROXY} }}\";" > /etc/apt/apt.conf.d/99pcache
+fi
+if [ -z "${_HTTP_PROXY}" ]; then
+  export http_proxy=${_HTTP_PROXY}
+fi
+
+# Clean
+rm -f *-stamp
+rm -rf build-*
+rm -rf output
+rm -f *.deb
+rm -f debian/*.debhelper
+rm -rf debian/.debhelper
+rm -f debian/*.log
+rm -f debian/*.substvars
+rm -rf debian/php-xxhash
+
+apt-get update
+apt-get install ${_APT_FLAGS_COMMON} ${_MAIN_PACKAGES}
+
+add-apt-repository -y -u ppa:ondrej/php
+
+# install build deb dependencies
+mk-build-deps -t "apt-get ${_APT_FLAGS_COMMON}" --install debian/control
+
+dpkg-buildpackage --root-command=fakeroot --build=binary --jobs=${_NB_PROC} --no-sign --unsigned-source --post-clean --pre-clean
---
+++ build_isolated.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -x
+
+_IMAGE_NAME=${IMAGE_NAME:=buildpack-deps}
+_IMAGE_VERSION=${IMAGE_VERSION:=18.04-scm}
+_PRIVATE_REPO=${PRIVATE_REPO}
+_APT_PROXY=${APT_PROXY}
+_HTTP_PROXY=${HTTP_PROXY}
+
+docker run --rm \
+    --interactive \
+    --workdir /workdir/debfiles-php-xxhash \
+    --volume $(pwd)/..:/workdir/ \
+    --env USERID=$(id -u) \
+    --env GROUPID=$(id -g) \
+    --env APT_PROXY=${_APT_PROXY} \
+    --env HTTP_PROXY=${_HTTP_PROXY} \
+    --entrypoint sh \
+  ${_PRIVATE_REPO}${_IMAGE_NAME}:${_IMAGE_VERSION} \
+    build.sh
